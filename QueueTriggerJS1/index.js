3205e9efe9a237210178ac44b9eedc328a498798
s0raySoA <so.dir228@gmail.com>
2018-10-31 11:10:13 +0900

    add WEATHER_ID

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -22,6 +22,63 @@ const MESSAGE_TYPE = {
   sticker: 'sticker',
 };
 
+const WEATHER_ID = {
+  200: '小雨と雷雨',
+  201: '雨と雷雨',
+  202: '大雨と雷雨',
+  210: '光雷雨',
+  211: '雷雨',
+  212: '重い雷雨',
+  221: 'ぼろぼろの雷雨',
+  230: '小雨と雷雨',
+  231: '霧雨と雷雨',
+  232: '重い霧雨と雷雨',
+  300: '光強度霧雨',
+  301: '霧雨',
+  302: '重い強度霧雨',
+  310: '光強度霧雨の雨',
+  311: '霧雨の雨',
+  312: '重い強度霧雨の雨',
+  313: 'にわかの雨と霧雨',
+  314: '重いにわかの雨と霧雨',
+  321: 'にわか霧雨',
+  500: '小雨',
+  501: '適度な雨',
+  502: '重い強度の雨',
+  503: '非常に激しい雨',
+  504: '極端な雨',
+  511: '雨氷',
+  520: '光強度のにわかの雨',
+  521: 'にわかの雨',
+  522: '重い強度にわかの雨',
+  531: '不規則なにわかの雨',
+  600: '小雪',
+  601: '雪',
+  602: '大雪',
+  611: 'みぞれ',
+  612: 'にわかみぞれ',
+  615: '光雨と雪',
+  616: '雨や雪',
+  620: '光のにわか雪',
+  621: 'にわか雪',
+  622: '重いにわか雪',
+  701: 'ミスト',
+  711: '煙',
+  721: 'ヘイズ',
+  731: '砂、ほこり旋回する',
+  741: '霧',
+  751: '砂',
+  761: 'ほこり',
+  762: '火山灰',
+  771: 'スコール',
+  781: '竜巻',
+  800: '晴天',
+  801: '薄い雲',
+  802: '雲',
+  803: '曇りがち',
+  804: '厚い雲',
+};
+
 /* url */
 const COGNITIVE_SERVICE = 'https://australiaeast.api.cognitive.microsoft.com/';
 
@@ -71,7 +128,7 @@ function postMessage(context, event) {
     }
   } catch (err) {
     context.log(err);
-    return client.replyMessage(event.replyToken, {
+    client.replyMessage(event.replyToken, {
       type: MESSAGE_TYPE.text,
       text: MSG_500,
     });
@@ -85,20 +142,32 @@ function postMessage(context, event) {
  * @param {*} event
  */
 function judgmentTextMessage(context, event) {
-  if (event.message.text.indexOf('天気') > -1) {
-    openWeatherMap(context, event)
-        .then((res) => {
-          context.log(res.weather[0]);
-          context.log(res.weather[0].main);
-          client.replyMessage(event.replyToken, {
-            type: MESSAGE_TYPE.text,
-            text: '東京の天気は、' + res.weather[0].main,
+  try {
+    if (event.message.text.indexOf('天気') > -1) {
+      openWeatherMap(context, event)
+          .then((res) => {
+            context.log(res.weather[0].id);
+            context.log(WEATHER_ID[res.weather[0].id]);
+            client.replyMessage(event.replyToken, [{
+              type: MESSAGE_TYPE.text,
+              text: '東京の天気は、' + WEATHER_ID[res.weather[0].id],
+            }, {
+              type: MESSAGE_TYPE.image,
+              originalContentUrl: 'https://openweathermap.org/img/w/' + res.weather[0].icon + '.png',
+              previewImageUrl: 'https://openweathermap.org/img/w/' + res.weather[0].icon + '.png',
+            }]);
           });
-        });
-  } else {
+    } else {
+      return client.replyMessage(event.replyToken, {
+        type: MESSAGE_TYPE.text,
+        text: MSG_400_1,
+      });
+    };
+  } catch (err) {
+    context.log(err);
     return client.replyMessage(event.replyToken, {
       type: MESSAGE_TYPE.text,
-      text: MSG_400_1,
+      text: MSG_500,
     });
   };
 };
