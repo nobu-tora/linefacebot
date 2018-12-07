6bab85deb287f1d8e48e91e36b64919e5b1958c9
odaira <odaira@012-R1812-003.local>
2018-12-07 15:59:46 +0900

    天気を削除する

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,13 +1,10 @@
 const https = require('https');
-var http = require('http');
 const url = require('url');
 const Client = require('@line/bot-sdk').Client;
 
 /* message */
 const MSG_400_1 = '画像をおくってね。\n顔年齢を診断できるよ！';
-const MSG_400_2 = 'このスタンプいいと思うよ！！';
-const MSG_400_3 = 'おやすみなさい';
-const MSG_400_4 = 'ほんとに顔写ってる画像？';
+const MSG_400_4 = 'ほんとに顔写ってる画像ですか？';
 const MSG_500 = '・・・エラーが発生しました。';
 
 /* ENUM */
@@ -22,65 +19,11 @@ const MESSAGE_TYPE = {
   sticker: 'sticker',
 };
 
-const WEATHER_ID = {
-  200: '小雨と雷雨',
-  201: '雨と雷雨',
-  202: '大雨と雷雨',
-  210: '光雷雨',
-  211: '雷雨',
-  212: '重い雷雨',
-  221: 'ぼろぼろの雷雨',
-  230: '小雨と雷雨',
-  231: '霧雨と雷雨',
-  232: '重い霧雨と雷雨',
-  300: '光強度霧雨',
-  301: '霧雨',
-  302: '重い強度霧雨',
-  310: '光強度霧雨の雨',
-  311: '霧雨の雨',
-  312: '重い強度霧雨の雨',
-  313: 'にわかの雨と霧雨',
-  314: '重いにわかの雨と霧雨',
-  321: 'にわか霧雨',
-  500: '小雨',
-  501: '適度な雨',
-  502: '重い強度の雨',
-  503: '非常に激しい雨',
-  504: '極端な雨',
-  511: '雨氷',
-  520: '光強度のにわかの雨',
-  521: 'にわかの雨',
-  522: '重い強度にわかの雨',
-  531: '不規則なにわかの雨',
-  600: '小雪',
-  601: '雪',
-  602: '大雪',
-  611: 'みぞれ',
-  612: 'にわかみぞれ',
-  615: '光雨と雪',
-  616: '雨や雪',
-  620: '光のにわか雪',
-  621: 'にわか雪',
-  622: '重いにわか雪',
-  701: 'ミスト',
-  711: '煙',
-  721: 'ヘイズ',
-  731: '砂、ほこり旋回する',
-  741: '霧',
-  751: '砂',
-  761: 'ほこり',
-  762: '火山灰',
-  771: 'スコール',
-  781: '竜巻',
-  800: '晴天',
-  801: '薄い雲',
-  802: '雲',
-  803: '曇りがち',
-  804: '厚い雲',
-};
+// Cognitive Service Region
+const COGNITIVE_SERVICE_REGION = process.env.COGNITIVE_SERVICE_REGION;
 
 /* url */
-const COGNITIVE_SERVICE = 'https://australiaeast.api.cognitive.microsoft.com/';
+const COGNITIVE_SERVICE = 'https://' + COGNITIVE_SERVICE_REGION + '.api.cognitive.microsoft.com/';
 
 /* LINE setting */
 const client = new Client({
@@ -95,9 +38,15 @@ const client = new Client({
  * @param {*} myQueueItem
  */
 module.exports = function(context, myQueueItem) {
-  myQueueItem.events.forEach((event) => postMessage(context, event));
-  context.log('done');
-  context.done();
+  var promises = [];
+  myQueueItem.events.forEach((event) => {
+    promises.push(postMessage(context, event));
+  });
+  Promise.all(promises)
+    .then(() => {
+      context.log('done');
+      context.done();
+    });
 };
 
 /**
@@ -107,97 +56,41 @@ module.exports = function(context, myQueueItem) {
  * @param {*} event
  */
 function postMessage(context, event) {
-  try {
-    context.log(event);
-    const type = event.message.type;
-    if (type === MESSAGE_TYPE.text) {
-      judgmentTextMessage(context, event);
-    } else if (type === MESSAGE_TYPE.image) {
-      getImageData(context, event)
-          .then((postData) => postCognitiveImage(context, event, postData));
-    } else if (type === MESSAGE_TYPE.sticker) {
-      client.replyMessage(event.replyToken, {
-        type: MESSAGE_TYPE.text,
-        text: MSG_400_2,
-      });
-    } else {
-      client.replyMessage(event.replyToken, {
-        type: MESSAGE_TYPE.text,
-        text: MSG_400_3,
-      });
-    }
-  } catch (err) {
-    context.log(err);
-    client.replyMessage(event.replyToken, {
-      type: MESSAGE_TYPE.text,
-      text: MSG_500,
-    });
-  };
-};
-
-/**
- * JudgmentTextMessage.
- *
- * @param {*} context
- * @param {*} event
- */
-function judgmentTextMessage(context, event) {
-  try {
-    if (event.message.text.indexOf('天気') > -1) {
-      openWeatherMap(context, event)
-          .then((res) => {
-            client.replyMessage(event.replyToken, [{
-              type: MESSAGE_TYPE.text,
-              text: '東京の天気は、' + WEATHER_ID[res.weather[0].id],
-            }, {
-              type: MESSAGE_TYPE.image,
-              originalContentUrl: 'https://openweathermap.org/img/w/' + res.weather[0].icon + '.png',
-              previewImageUrl: 'https://openweathermap.org/img/w/' + res.weather[0].icon + '.png',
-            }]);
-          });
-    } else {
-      return client.replyMessage(event.replyToken, {
-        type: MESSAGE_TYPE.text,
-        text: MSG_400_1,
-      });
-    };
-  } catch (err) {
-    context.log(err);
-    return client.replyMessage(event.replyToken, {
-      type: MESSAGE_TYPE.text,
-      text: MSG_500,
-    });
-  };
-};
-
-/**
- * openWeatherMap
- *
- * @param {*} context
- * @param {*} event
- */
-function openWeatherMap(context, event) {
   return new Promise((resolve, reject) => {
-    var location = 'Tokyo,jp';
-    var units = 'metric';
-    var APIKEY = process.env.OPEN_WEATHER_MAP_KEY;
-    var URL = 'http://api.openweathermap.org/data/2.5/weather?q='+ location +'&units='+ units +'&appid='+ APIKEY;
-
-    http.get(URL, function(res) {
-      var body = '';
-      res.setEncoding('utf8');
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      res.on('data', function(chunk) {
-        res = JSON.parse(body);
-        context.log(res);
-        resolve(res);
-      });
-    }).on('error', function(e) {
-      context.log(e.message);
-      reject(e.message);
-    });
+    try {
+      context.log(event);
+      const type = event.message.type;
+      if (type === MESSAGE_TYPE.image) {
+        getImageData(context, event)
+            .then((postData) => postCognitiveImage(context, event, postData))
+            .then((faceData) => {
+              var isExistFace = faceData.length !== 0 ? true : false;
+              if (isExistFace) {
+                successReplyMessage(context, event, faceData);
+              } else {
+                client.replyMessage(event.replyToken, {
+                  type: MESSAGE_TYPE.text,
+                  text: MSG_400_4,
+                });
+              };
+            })
+            .then(()=> resolve());
+      } else {
+        context.log('[log] No image');
+        client.replyMessage(event.replyToken, {
+          type: MESSAGE_TYPE.text,
+          text: MSG_400_1,
+        })
+        .then(()=> resolve());
+      }
+    } catch (err) {
+      context.log(err);
+      client.replyMessage(event.replyToken, {
+        type: MESSAGE_TYPE.text,
+        text: MSG_500,
+      })
+      .then(()=> resolve());
+    };
   });
 };
 
@@ -209,6 +102,7 @@ function openWeatherMap(context, event) {
  */
 function getImageData(context, event) {
   return new Promise((resolve, reject) => {
+    context.log('[log] Start getImageData');
     var data = [];
     client.getMessageContent(event.message.id)
         .then((stream) => {
@@ -220,11 +114,14 @@ function getImageData(context, event) {
             reject(err);
           });
           stream.on('end', () => {
-            context.log('get image');
+            context.log('[log] Success getImageData');
             resolve(Buffer.concat(data));
           });
         })
-        .catch((err) => reject(err));
+        .catch((err) => {
+          context.log('[err] postCognitiveImage');
+          reject(err);
+        });
   });
 };
 
@@ -236,49 +133,74 @@ function getImageData(context, event) {
  * @param {*} postData
  */
 function postCognitiveImage(context, event, postData) {
-  var parseUrl = url.parse(COGNITIVE_SERVICE + 'face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile');
-  var postOptions = {
-    host: parseUrl.host,
-    path: parseUrl.path,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/octet-stream',
-      'Content-Length': postData.length,
-      'Ocp-Apim-Subscription-Key': process.env.COGNITIVE_KEY,
-    },
-  };
+  return new Promise((resolve, reject) => {
+    try {
+      context.log('[log] Start postCognitiveImage');
+      var parseUrl = url.parse(COGNITIVE_SERVICE + 'face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile');
+      var postOptions = {
+        host: parseUrl.host,
+        path: parseUrl.path,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'Content-Length': postData.length,
+          'Ocp-Apim-Subscription-Key': process.env.COGNITIVE_KEY,
+        },
+      };
 
-  var bodyString = null;
-  var req = https.request(postOptions, function(res) {
-    res.setEncoding('utf8');
-    res.on('data', function(chunk) {
-      bodyString = chunk;
-    });
-    res.on('end', function() {
-      if (res.statusCode !== 200 || bodyString === '[]') {
-        return client.replyMessage(event.replyToken, {
-          type: MESSAGE_TYPE.text,
-          text: MSG_400_4,
+      var bodyString = null;
+      var req = https.request(postOptions, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+          bodyString = chunk;
         });
-      } else {
-        var result = JSON.parse(bodyString);
-        context.log(result[0]);
-        var age = result[0].faceAttributes.age;
-        var gender = '人間';
-
-        if (result[0].faceAttributes.gender === 'male') {
-          gender = ENUM_GENDER.male;
-        } else if (result[0].faceAttributes.gender === 'female') {
-          gender = ENUM_GENDER.female;
-        };
-        context.log('success post line');
-        return client.replyMessage(event.replyToken, {
-          type: MESSAGE_TYPE.text,
-          text: 'う～ん、、、\n' + age + '歳の' + gender + 'かな？',
+        res.on('end', function() {
+          if (res.statusCode !== 200 || bodyString === '[]') {
+            context.log('[log] No face');
+            resolve([]);
+          } else {
+            context.log('[log] Success postCognitiveImage');
+            resolve(bodyString);
+          };
         });
-      }
-    });
+      });
+      req.write(postData);
+      req.end();
+    } catch (err) {
+      context.log('[err] postCognitiveImage');
+      reject(err);
+    };
   });
-  req.write(postData);
-  req.end();
+};
+
+/**
+ * success
+ *
+ * @param {*} context
+ * @param {*} event
+ * @param {*} faceData
+ */
+function successReplyMessage(context, event, faceData) {
+  return new Promise((resolve, reject) => {
+    try {
+      var face = JSON.parse(faceData)[0];
+      var age = face.faceAttributes.age;
+      var gender = '人間';
+
+      if (face.faceAttributes.gender === 'male') {
+        gender = ENUM_GENDER.male;
+      } else if (face.faceAttributes.gender === 'female') {
+        gender = ENUM_GENDER.female;
+      };
+
+      client.replyMessage(event.replyToken, {
+        type: MESSAGE_TYPE.text,
+        text: 'う～ん、、、\n' + age + '歳の' + gender + 'かな？',
+      })
+      .then(() => resolve());
+    } catch (err) {
+      context.log('[err] successReplyMessage');
+      reject(err);
+    };
+  });
 };
